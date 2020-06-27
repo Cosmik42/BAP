32ae87ff6d604b32830668d45e601f54dbeb657e
Cosmik42 <vincent@luckyduckgames.com>
2020-06-27 23:31:33 +0200

    Some initial clean up
    Ignore .bin
    Ignore obj
    Ignore .vs
    
    Remove donation button

--- LTP.Desktop/Main UI/FormWelcome.Designer.cs
+++ LTP.Desktop/Main UI/FormWelcome.Designer.cs
@@ -30,7 +30,6 @@
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormWelcome));
 			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-			this.buttonDonate = new System.Windows.Forms.Button();
 			this.button1 = new System.Windows.Forms.Button();
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
@@ -43,33 +42,23 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-			this.richTextBox1.Location = new System.Drawing.Point(13, 173);
-			this.richTextBox1.Margin = new System.Windows.Forms.Padding(10);
+			this.richTextBox1.Location = new System.Drawing.Point(10, 141);
+			this.richTextBox1.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
 			this.richTextBox1.Name = "richTextBox1";
 			this.richTextBox1.RightMargin = 500;
 			this.richTextBox1.ShowSelectionMargin = true;
-			this.richTextBox1.Size = new System.Drawing.Size(644, 325);
+			this.richTextBox1.Size = new System.Drawing.Size(483, 264);
 			this.richTextBox1.TabIndex = 0;
 			this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
 			this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
 			// 
-			// buttonDonate
-			// 
-			this.buttonDonate.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-			this.buttonDonate.Location = new System.Drawing.Point(542, 13);
-			this.buttonDonate.Name = "buttonDonate";
-			this.buttonDonate.Size = new System.Drawing.Size(101, 49);
-			this.buttonDonate.TabIndex = 1;
-			this.buttonDonate.Text = "Donate to Support";
-			this.buttonDonate.UseVisualStyleBackColor = true;
-			this.buttonDonate.Click += new System.EventHandler(this.buttonDonate_Click);
-			// 
 			// button1
 			// 
 			this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-			this.button1.Location = new System.Drawing.Point(542, 68);
+			this.button1.Location = new System.Drawing.Point(407, 11);
+			this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(101, 33);
+			this.button1.Size = new System.Drawing.Size(76, 27);
 			this.button1.TabIndex = 2;
 			this.button1.Text = "Close";
 			this.button1.UseVisualStyleBackColor = true;
@@ -78,9 +67,10 @@
 			// pictureBox1
 			// 
 			this.pictureBox1.Image = global::LegoTrainProject.Properties.Resources._2019_02_09_17_51_12;
-			this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+			this.pictureBox1.Location = new System.Drawing.Point(10, 11);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(178, 147);
+			this.pictureBox1.Size = new System.Drawing.Size(134, 119);
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.pictureBox1.TabIndex = 3;
 			this.pictureBox1.TabStop = false;
@@ -91,25 +81,25 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-			this.richTextBox2.Location = new System.Drawing.Point(204, 13);
-			this.richTextBox2.Margin = new System.Windows.Forms.Padding(10);
+			this.richTextBox2.Location = new System.Drawing.Point(153, 11);
+			this.richTextBox2.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
 			this.richTextBox2.Name = "richTextBox2";
 			this.richTextBox2.ShowSelectionMargin = true;
-			this.richTextBox2.Size = new System.Drawing.Size(325, 147);
+			this.richTextBox2.Size = new System.Drawing.Size(244, 119);
 			this.richTextBox2.TabIndex = 4;
 			this.richTextBox2.Text = resources.GetString("richTextBox2.Text");
 			// 
 			// FormWelcome
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(668, 517);
+			this.ClientSize = new System.Drawing.Size(501, 420);
 			this.Controls.Add(this.richTextBox2);
 			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.button1);
-			this.Controls.Add(this.buttonDonate);
 			this.Controls.Add(this.richTextBox1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.Name = "FormWelcome";
 			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -122,7 +112,6 @@
 		#endregion
 
 		private System.Windows.Forms.RichTextBox richTextBox1;
-		private System.Windows.Forms.Button buttonDonate;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.RichTextBox richTextBox2;
